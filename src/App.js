280168240667b77af65c0c6e4689bf9c3335a7fc
Danish Shafi <danish.shafi@live.co.uk>
2023-08-15 17:34:46 +0100

    useState to change value based on event onChange

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import StateCounter from './hooks/useState/StateCounter';
+import StateName from './hooks/useState/StateName';
 
 function App() {
   return (
     <div className="App">
-      <StateCounter />
+      {/* <StateCounter /> */}
+      <StateName />
     </div>
   );
 }
--- src/hooks/useState/StateName.js
+++ src/hooks/useState/StateName.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const StateName = () => {
+
+  const [inputVal, setInputVal] = useState("Dan");
+
+  const nameChange = (e) => {
+    let newVal = e.target.value;
+    setInputVal(newVal);
+  }
+
   return (
     <div>
-
+      <input
+        onChange={nameChange}
+        type="text"
+        placeholder='Enter something'
+      />
+      {inputVal}
     </div>
   )
 }
