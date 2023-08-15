ceb8a31dcd24a1a64930bcbfb4ccc35edea84b27
Danish Shafi <danish.shafi@live.co.uk>
2023-08-15 18:41:14 +0100

    useReducer hook created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ReducerCounter from './hooks/useReducer/ReducerCounter';
 import StateCounter from './hooks/useState/StateCounter';
 import StateName from './hooks/useState/StateName';
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <div className="App">
       {/* <StateCounter /> */}
-      <StateName />
+      {/* <StateName /> */}
+      <ReducerCounter />
     </div>
   );
 }
